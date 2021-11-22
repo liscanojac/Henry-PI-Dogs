b22fad787f92863f7d6963b5d49e21ed4de68785
Juan Canelon <liscanojac@gmail.com>
2021-11-22 19:20:23 -0300

    Implementing image addition to dogs in DB

--- api/src/routes/dog.js
+++ api/src/routes/dog.js
@@ -6,13 +6,29 @@ router.post('/', async (req, res) => {
 
   const { name, height, weight, life_span, temperament } = req.body;
 
+  var images = [
+        "https://images.pexels.com/photos/2797318/pexels-photo-2797318.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/2007/animal-dog-pet-cute.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/3433366/pexels-photo-3433366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/7444093/pexels-photo-7444093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/4529775/pexels-photo-4529775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/7112208/pexels-photo-7112208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/4224125/pexels-photo-4224125.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/5205386/pexels-photo-5205386.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/7210631/pexels-photo-7210631.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/6542554/pexels-photo-6542554.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      ]
+  
+  var randomNum = Math.floor(Math.random() * 11);
+
   try {
 
     const newDog = await Dog.create({
       name,
       height,
       weight,
-      life_span
+      life_span,
+      image: images[randomNum]
     });
 
     newDog.addTemperaments(temperament);
--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -92,23 +92,6 @@ const getDbDogs = async () => {
         avgWeight = Number(avgWeight[0]);
       }
 
-      var images = [
-        "https://images.pexels.com/photos/2797318/pexels-photo-2797318.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/2007/animal-dog-pet-cute.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/3433366/pexels-photo-3433366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/7444093/pexels-photo-7444093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/4529775/pexels-photo-4529775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/7112208/pexels-photo-7112208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        "https://images.pexels.com/photos/4224125/pexels-photo-4224125.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        "https://images.pexels.com/photos/5205386/pexels-photo-5205386.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        "https://images.pexels.com/photos/7210631/pexels-photo-7210631.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        "https://images.pexels.com/photos/6542554/pexels-photo-6542554.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-      ]
-      // var image = "https://images.pexels.com/photos/6542554/pexels-photo-6542554.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-
-      var randomNum = Math.floor(Math.random() * 11);
-      var randomImg = images[randomNum];
-
       var dogDetails = {
         id : dogDB.id,
         name : dogDB.name,
@@ -118,8 +101,7 @@ const getDbDogs = async () => {
         life_span: dogDB.life_span,
         created_in_db: dogDB.created_in_db,
         temperament: temperamentStr,
-        // image: dogDB.image
-        image: randomImg
+        image: dogDB.image
       };
       formattedDogsDB.push(dogDetails);
     }

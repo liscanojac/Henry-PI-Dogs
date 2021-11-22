c796a0697a55c31343e3a7fbdb3e394fef9cbafb
Juan Canelon <liscanojac@gmail.com>
2021-11-22 19:39:04 -0300

    Adding module CSS styling to LandingPage Component

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css"
+import landingPageImg from "./images/victor-grabarczyk-2pbnDRhXc6Q-unsplash.jpg"
 
 export default function LandingPage() {
   return (
     <div className={`${styles.body} grilla`}>
-      <div>
-        <h1>Welcome to PI-Dogs</h1>
-        <Link to='/home'>
-          <button>Enter</button>
-        </Link>
+      <div className="flex flex-column">
+        {/* <h1 className="special-heading">Welcome to PI-Dogs</h1> */}
+        <ul className={`special-heading ${styles.heading}`}>
+          <li><h1>Who let</h1></li>
+          <li><h1>the Dogs out?</h1></li>
+        </ul>
+        <div className="w-100">
+          <div className={`flex flex-start ${styles.landingPageDescription}`}>
+            <p>A Dogs PI for Henry</p>
+          </div>
+        </div>
+        <div className="w-100">
+          <div className="flex flex-end">
+            <Link to='/home'>
+              <button className={`boton ${styles.landingPageBtn}`}>Enter</button>
+            </Link>
+          </div>
+        </div>              
       </div>
-      <div className={styles.dogBg}>
-        {/* <img src="/images/pexels-pixabay-65928.jpg" alt="" /> */}
+      <div className={`${styles.dogBg} flex flex-start`}>
+        <div className={styles.imgContainer}>
+          <img className={styles.dogImg} src={landingPageImg} alt="" />
+        </div>
+        
+        {/* <div className={styles.dogBg}>
+          <img src={landingPageImg} alt="" />
+        </div> */}
+        {/* <div className={styles.dogBg}></div> */}
+        
       </div>
     </div>
   );

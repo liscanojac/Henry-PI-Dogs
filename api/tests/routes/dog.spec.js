be5f5c756ff90a21cce6377de724af971eb10487
Juan Canelon <liscanojac@gmail.com>
2021-11-23 15:29:24 -0300

    Implementing routes testing

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -12,6 +12,19 @@ const dog = {
   life_span: '15'
 };
 
+const newDog = {
+  name: 'Chihuahua',
+  height: '10 - 14',
+  weight: '8 - 15',
+  life_span: '16'
+}
+
+const newDogWithoutHeight = {
+  name: 'Shiba',
+  weight: '7 - 12',
+  life_span: '18'
+}
+
 describe('Videogame routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
@@ -25,3 +38,60 @@ describe('Videogame routes', () => {
     );
   });
 });
+
+describe('Routes', () => {
+
+  describe('GET /dogs', () => {
+    it('expecting 200 as response', () =>
+      agent.get('/dogs').expect(200)
+    );
+  });
+
+  describe('GET /dogs?name=terrier', () => {
+    it('expecting 200 when terrier is the name searched', () => 
+      agent.get('/dogs?name=terrier').expect(200)
+    );
+  });
+
+  describe('GET /dogs?name=nonExistingName', () => {
+    it('expecting 404 when the name searched doesnt exist', () => 
+      agent.get('/dogs?name=nonExistingName').expect(404)
+    );
+  });
+
+  describe('GET /dogs/:id', () => {
+    it('expecting 200 when the id searched exists', () => 
+      agent.get('/dogs/4').expect(200)
+    );
+  });
+
+  describe('GET /dogs/:id', () => {
+    it('expecting 404 when the id searched doesnt exist', () => 
+      agent.get('/dogs/304').expect(404)
+    );
+  });
+
+  describe('GET /temperament', () => {
+    it('expecting 200 as response', () => 
+      agent.get('/temperament').expect(200)
+    );
+  });
+
+  describe('GET /nonExistingRoute', () => {
+    it('expecting 404 as response', () => 
+      agent.get('/nonExistingRoute').expect(404)
+    );
+  });
+
+  describe('POST /dog', () => {
+    it('expecting 200 as response with a successful post', () => 
+      agent.post('/dog').send(newDog).expect(200)
+    );
+  });
+
+  describe('POST /dog', () => {
+    it('expecting 400 as response with a unsuccessful post', () => 
+      agent.post('/dog').send(newDogWithoutHeight).expect(400)
+    );
+  });
+})

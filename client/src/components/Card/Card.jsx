4c4fff67f788c0e617393263739f554bc5a243ea
Juan Canelon <liscanojac@gmail.com>
2021-11-23 16:59:52 -0300

    Improving card image sizing and link

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Card.css";
 
-export default function Card({ name, image, temperament, weight }) {
+export default function Card({ name, image, temperament, weight, key }) {
   
   return (
     // <div>
@@ -11,18 +12,21 @@ export default function Card({ name, image, temperament, weight }) {
     //   <p>{weight}</p>
     // </div>
     <div className="carta">
-      <h3 className="centrar-texto">{name}</h3>
-      <div className="grilla">
-        <div className="imgContainer">
-          <img src={image} alt="" />
-        </div>
-        <div>
-          <h4>Weight</h4>
-          <p>{weight} Kgs</p>
-          <h4>Temperament:</h4>
-          <p>{temperament}</p>
-        </div>
-      </div>
+      <Link key={key} className="cardLink" to={"/dogs/" + {key}}>
+        <h3 className="centrar-texto">{name}</h3>
+        <div className="grilla">
+          <div className="imgContainer">
+            <img src={image} alt="" />
+          </div>
+          <div>
+            <h4>Weight</h4>
+            <p>{weight} Kgs</p>
+            <h4>Temperament:</h4>
+            <p>{temperament}</p>
+          </div>
+        </div>                
+      </Link>
+      
     </div>
   );
 }
